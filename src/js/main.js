e618c73b3166b2f8281acdb9d3d8a6649a3ed656
Aaron <aaron@countable.ca>
2018-09-10 10:10:09 -0700

    fix ursl

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,53 @@
-const helloworld = () => {
-  console.log('Hello');
-};
+import Vue from 'vue/dist/vue';
 
-helloworld();
-
-async function fetchJoke() {
-  console.log('Fetching Joke..');
-
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-  return response.json();
-}
-
-(async function getJoke() {
-  try {
-    const joke = await fetchJoke();
-    console.log('Joke Should Display?');
-    console.log(JSON.stringify(joke));
-    console.log(joke);
-  } catch (e) {
-    console.log(e);
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
   }
-})();
+});
 
-(function one() {
-  return one;
-})();
+new Vue({
+  el: '#app2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+});
+
+new Vue({
+  el: '#app3',
+  data: {
+    seen: true
+  }
+});
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('');
+    }
+  }
+});
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+});
